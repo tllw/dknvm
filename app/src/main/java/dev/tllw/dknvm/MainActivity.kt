592733294844649a072f1a8ca8a73758d4ed55c7
tllw <miroslav.dobrev@gmail.com>
2020-03-19 16:10:08 +0200

    Initial project setup

---
+++ app/src/main/java/dev/tllw/dknvm/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.tllw.dknvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
